bd32ce2d643fd211104ddfdfa8c0bd286e16c7fb
suraj1672 <53619986+suraj1672@users.noreply.github.com>
2023-06-12 16:19:55 +0530

    Update test_test1.py

--- test_test1.py
+++ test_test1.py
@@ -1,3 +1,7 @@
-def test_mock():
-  return "This web app made by suraj"
-  assert True
+from flask import Flask 
+app = flask(_name_)
+
+
+@app.route("/")
+def home():
+  return "Hello this code was updated locally and pushed to Azure Devops repo"
