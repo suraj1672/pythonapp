a89d07427fa48c1ddc5ce6d5cb7baf2dc393a5f5
suraj1672 <53619986+suraj1672@users.noreply.github.com>
2023-06-12 16:28:23 +0530

    Update webapp.py

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -1,6 +1,7 @@
-# Entry point for the application.
-from . import app    # For application discovery by the 'flask' command. 
-from . import views  # For import side-effects of setting up routes. 
+from flask import Flask 
+app = flask(_name_)
 
-# Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
-# print('http://127.0.0.1:5000/hello/VSCode')
+
+@app.route("/")
+def home():
+  return "Hello this code was updated locally and pushed to Azure Devops repo"
